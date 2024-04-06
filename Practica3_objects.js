0a688b7283a728427641a849255722e2c769c910
JoseSoto <jose.sotoo@hotmail.om>
2024-04-06 13:28:14 -0700

    feature added: Practica3_objects.js

---
+++ Practica3_objects.js
@@ -0,0 +1,17 @@
+//Sistema de gestion de libros
+
+const libros = {
+    titulo: "El Psicoanalista",
+    autor: "John Katzenbach",
+    genero: "Thriller",
+    publicacion: "09/09/2009",
+    editorial: "Ediciones B",
+}
+
+console.log(`El autor del libro es${libros.autor}`);
+
+
+
+
+
+
